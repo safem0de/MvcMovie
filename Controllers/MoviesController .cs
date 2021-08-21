0f923c53da72a822063015e3dc77d6c593e8eb63
Naweeparb Wangwanich <n.wangwanich@gmail.com>
2021-08-21 14:20:38 +0700

    Index+Details Controller

--- Controllers/MoviesController .cs
+++ Controllers/MoviesController .cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 
 namespace MvcMovie.Controllers
@@ -10,6 +12,29 @@ namespace MvcMovie.Controllers
         public MoviesController(MvcMovieContext context)
         {
             _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movie.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
         }
     }
 }
