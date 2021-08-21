b1193970e6b6c5ca60f0878771600a05a0b642a2
Naweeparb Wangwanich <n.wangwanich@gmail.com>
2021-08-21 16:01:51 +0700

    21-08-21

--- Controllers/MoviesController .cs
+++ Controllers/MoviesController .cs
@@ -25,18 +25,18 @@ namespace MvcMovie.Controllers
         //     return View(await _context.Movie.ToListAsync());
         // }
 
-        public async Task<IActionResult> Index(string searchString)
-        {
-            var movies = from m in _context.Movie
-                        select m;
+        // public async Task<IActionResult> Index(string searchString)
+        // {
+        //     var movies = from m in _context.Movie
+        //                 select m;
 
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                movies = movies.Where(s => s.Title.Contains(searchString));
-            }
+        //     if (!String.IsNullOrEmpty(searchString))
+        //     {
+        //         movies = movies.Where(s => s.Title.Contains(searchString));
+        //     }
 
-            return View(await movies.ToListAsync());
-        }
+        //     return View(await movies.ToListAsync());
+        // }
 
         public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
