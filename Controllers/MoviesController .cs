c8efe116f8e2b444d5173e02f8108b3435bc6168
Naweeparb Wangwanich <n.wangwanich@gmail.com>
2021-08-21 15:09:14 +0700

    Searching Genre

--- Controllers/MoviesController .cs
+++ Controllers/MoviesController .cs
@@ -6,6 +6,7 @@ using MvcMovie.Data;
 using MvcMovie.Models;
 
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MvcMovie.Controllers
 {
@@ -35,6 +36,35 @@ namespace MvcMovie.Controllers
             }
 
             return View(await movies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                        select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         [HttpPost]
