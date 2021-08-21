60d8ba8d11137abd86444ef7dfdfd227b37b6206
Naweeparb Wangwanich <n.wangwanich@gmail.com>
2021-08-21 14:09:47 +0700

    Movie Controller

---
+++ Controllers/MoviesController .cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Data;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+    }
+}
