cdcb752dba7aeef08796341c872a0fab61b22d52
safem0de <n.wangwanich@gmail.com>
2021-08-23 14:23:09 +0700

    Update Rating

--- Migrations/20210823071004_InitialCreate.Designer.cs
+++ Migrations/20210823071004_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20210821070120_InitialCreate")]
+    [Migration("20210823071004_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -28,6 +28,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
